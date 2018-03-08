ef26793f0c789d45444ff6924c2b7e5c9d252f1c
Michał Soida <msoida@users.noreply.github.com>
2018-03-08 20:31:00 +0100

    small fix

--- michalsoida/database/__init__.py
+++ michalsoida/database/__init__.py
@@ -1,6 +1,6 @@
-from peewee import DoesNotExist
+from peewee import DoesNotExist, JOIN
 
-from .peewee import db, JOIN
+from .peewee import db
 from .auth import User
 from .project import Project
 from .tracking import PrintStorage, PostgresStorage, Tracking
