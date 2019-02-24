496d3f35d47d3645e1e6c84eff38d4862ee1962a
Michał Soida <msoida@users.noreply.github.com>
2019-02-24 14:38:13 +0100

    Code cleanup

--- michalsoida/frontend/blueprint.py
+++ michalsoida/frontend/blueprint.py
@@ -1,14 +1,9 @@
-from datetime import datetime
-
 from flask import (Blueprint, render_template, make_response,
                    url_for, request, abort, flash, redirect)
 from flask_login import (current_user, login_required, login_url)
-from pytz import timezone
 
 from ..database import Project
 
-
-tz = timezone('Europe/Warsaw')
 
 frontend = Blueprint('frontend', __name__, template_folder='templates')
 
@@ -33,11 +28,6 @@ def kontakt():
     return render_template('frontend/kontakt.html')
 
 
-@frontend.route('/pgp/')
-def pgp():
-    return render_template('frontend/pgp.html')
-
-
 @frontend.route('/projekty/')
 def projekty():
     projects = Project.select().order_by(Project.title)
@@ -46,8 +36,7 @@ def projekty():
 
 @frontend.route('/o-stronie/')
 def o_stronie():
-    t = datetime.now(tz).strftime('%d.%m.%Y %H:%M:%S')
-    return render_template('frontend/o-stronie.html', data_godzina=t)
+    return render_template('frontend/o-stronie.html')
 
 
 @frontend.route('/apple-touch-icon-precomposed.png')
@@ -68,24 +57,3 @@ def keybase_txt():
 @frontend.route('/index.php')
 def red_index():
     return redirect(url_for('index'))
-
-
-@frontend.route('/pogoda/')
-@frontend.route('/download/')
-@frontend.route('/html2text/')
-@frontend.route('/ipogoda/')
-@frontend.route('/ipogoda/image.php')
-@frontend.route('/ipogoda/index.html')
-@frontend.route('/ipogoda/komentarz.php')
-@frontend.route('/ipogoda/makeimage.php')
-@frontend.route('/fuzzer/')
-@frontend.route('/fuzzer/status')
-@frontend.route('/covers/')
-@frontend.route('/covers/form/')
-@frontend.route('/covers/i/')
-@frontend.route('/iwebkit/')
-@frontend.route('/rsscovers/')
-@frontend.route('/rmf/')
-@frontend.route('/skaner/')
-def file_deleted():
-    abort(410)
