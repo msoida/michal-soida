1494ee39a02593916da58a4a7bd06b801b851ad8
Michał Soida <msoida@users.noreply.github.com>
2019-02-26 10:12:10 +0100

    Added new Project definition

--- michalsoida/database/project.py
+++ michalsoida/database/project.py
@@ -1,12 +1,20 @@
-from peewee import TextField
+from peewee import TextField, BlobField
 
 from .peewee import BaseModel
 
 
 class Project(BaseModel):
-    title = TextField()
-    description = TextField()
+    title_pl = TextField()
+    title_en = TextField()
 
+    description_pl = TextField()
+    description_en = TextField()
+
+    thumbnail = BlobField(null=True)
+    thumbnail_mimetype = TextField(null=True)
+
+    content_pl = TextField()
+    content_en = TextField()
 
 tables_to_create = [
     Project,
